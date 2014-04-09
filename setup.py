68d585ac489ee12c43588ee333059adb44819503
Robert Elwell <robert@wikia-inc.com>
2014-04-09 14:41:36 -0700

    what up with setup

--- setup.py
+++ setup.py
@@ -7,11 +7,10 @@ from setuptools import setup
 setup(
     name=u'wikia_solr_backend',
     version=u'0.0.1',
-    author=u'Wikia Platform Team',
-    author_email=u'platform-l@wikia-inc.com',
+    author=u'Robert Elwell',
+    author_email=u'robert@wikia-inc.com',
     description=u'Wikia Deploy Tools',
     license=u'Other',
-    test_suite=u'deploytools.test',
     install_requires=[u"requests", u"wikiautils"],
     packages=[u'wikia_solr_backend'],
 )
